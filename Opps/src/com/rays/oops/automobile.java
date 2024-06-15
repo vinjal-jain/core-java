1fe656ea9b387047247423f71b5de47714c77c95
vinjal-jain <vinjaljain14it@gmail.com>
2024-06-15 14:29:27 +0530

    oopsBegin

---
+++ Opps/src/com/rays/oops/automobile.java
@@ -0,0 +1,48 @@
+package com.rays.oops;
+
+public class automobile {
+		
+		String carname = "" ;
+		String colour ="" ;
+		String modeldetails = "";
+	    String horsepower = "";
+	    int speedupto = 0;
+		int gear = 0;
+		public String getCarname() {
+			return carname;
+		}
+		public void setCarname(String carname) {
+			this.carname = carname;
+		}
+		public String getColour() {
+			return colour;
+		}
+		public void setColour(String colour) {
+			this.colour = colour;
+		}
+		public String getModeldetails() {
+			return modeldetails;
+		}
+		public void setModeldetails(String modeldetails) {
+			this.modeldetails = modeldetails;
+		}
+		public String getHorsepower() {
+			return horsepower;
+		}
+		public void setHorsepower(String horsepower) {
+			this.horsepower = horsepower;
+		}
+		public int getSpeedupto() {
+			return speedupto;
+		}
+		public void setSpeedupto(int speed) {
+			this.speedupto = speedupto;
+		}
+		public int getGear() {
+			return gear;
+		}
+		public void setGear(int gear) {
+			this.gear = gear;
+		}
+		
+}
---
+++ Opps/src/com/rays/oops/testAutomobile.java
@@ -0,0 +1,53 @@
+package com.rays.oops;
+
+public class testAutomobile {
+	public static void main(String[] args) {
+		
+		automobile a = new automobile();
+		
+		a.setCarname("MUSTANG 1969");
+		a.setColour("Black");
+		a.setModeldetails("The 1969 model year restyle added more heft to the body\" with body length extended by 3.8 inches (97 mm) (the wheelbase remaining at 108 inches), width increased by almost half an inch, and the Mustang's \"weight went up markedly too\". 1969 was the first model to use quad headlamps placed both inside and outside the grille opening. The corralled grille pony was replaced with the pony and tribars logo, set off-center to the drivers side. The car was longer than previous models and sported convex rather than concave side panels. The fastback body version was renamed Sportsroof, styled as SportsRoof in Ford's literature.");
+		a.setHorsepower("335 hp at 5200 rpm");
+		a.setSpeedupto(450);
+		a.setGear(6);
+		
+		System.out.println("Carname:-" + a.getCarname());
+		System.out.println("Colour:-"+ a.getColour());
+		System.out.println("Details:-"+ a.getModeldetails());
+		System.out.println("Horsepower:-"+ a.getHorsepower());
+		System.out.println("speedupto:-"+ a.getSpeedupto());
+		System.out.println("Total Gear in this monster is :-"+ a.getGear() );
+		
+		
+		a.setGear(3);
+		
+		System.out.println("Monster in gear:-" + a.gear);
+		
+		if (a.getGear() == 1) {
+			
+			System.out.println("Getting Warm up");
+			
+		}else if (a.getGear()== 2 ) {
+			
+			System.out.println("Ready to go fly");
+		}else if (a.getGear()== 3 ) {
+			
+			System.out.println("Taking off ");
+			
+		}else if (a.getGear()== 4 ) {
+			
+			System.out.println("Flying mode");
+			
+		}else if (a.getGear()== 5 ) {
+			
+			System.out.println("Beast Boost");
+			
+		}else if (a.getGear()== -1 ) {
+			
+			System.out.println("piche nikal le ");
+		}
+		
+	}
+
+}
